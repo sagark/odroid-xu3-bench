73ead6fbbbf5fae6e678f7f0d2e29e460912fcf4
Sagar Karandikar <karandikarsagar@gmail.com>
2015-05-28 10:47:28 +0000

    add data for all working parboil benchmarks

--- parboil/run-all.sh
+++ parboil/run-all.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-benchmarks=(bfs stencil tpacf cutcp mri-gridding sad spmv histo sgemm mri-q lbm)
-version=(opencl_base opencl_base opencl_base opencl_base opencl_base opencl_base opencl_base opencl_base opencl_base opencl opencl_base) 
+# benchmarks that are currently broken: (lbm, opencl_base, long or short), (tpacf, opencl_base, small/medium/large), (mri-gridding, opencl_base, small), (histo, opencl_base, small/default), (sad, opencl_base, large/default)
 
-dset=(1M default large large small large large large medium large long)
+benchmarks=(bfs stencil cutcp spmv sgemm mri-q)
+version=(opencl_base opencl_base opencl_base opencl_base opencl_base opencl) 
+
+dset=(1M default large large medium large)
 
 for ((i=0;i<${#benchmarks[@]};++i)); do
 	echo "${benchmarks[i]} -------------------------------"
